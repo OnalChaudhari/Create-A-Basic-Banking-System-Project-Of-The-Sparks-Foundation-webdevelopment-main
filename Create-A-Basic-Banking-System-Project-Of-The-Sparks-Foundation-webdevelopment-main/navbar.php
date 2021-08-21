656768ea6b47090d3af2c94c515b1d7b7a2a508f
OnalChaudhari <59107911+OnalChaudhari@users.noreply.github.com>
2021-08-21 16:50:52 +0530

    Add files via upload

---
+++ Create-A-Basic-Banking-System-Project-Of-The-Sparks-Foundation-webdevelopment-main/navbar.php
@@ -0,0 +1,88 @@
+<!-- navbar -->
+      <!DOCTYPE html>
+      <html>
+      <head>
+
+        <style type="text/css">
+
+        body{margin-top: 15px;
+		background-color: lavender;
+        }
+		.collapse navbar-collapse
+		{
+			background-color:#333;
+			overflow:hidden;
+
+		}
+		.collapse navbar-collapse a
+		{
+			float :left;
+			color : #f2f2f2;
+			text-align : center;
+			padding : 14px 16px;
+			text-decoration:none;
+			font-size:17px;
+
+
+
+		}
+          .btn {
+                  border: 2px solid black;
+                  background-color: #333;
+                  color: #f2f2f2;
+                  padding: 14px 25px;
+                  font-size: 16px;
+                  cursor: pointer;
+                  margin: 6px;
+                }
+
+            .df:hover {
+
+         color: white;
+        font-size: 16px;
+        background-color: #808080;
+
+}
+#collapsibleNavbar
+{
+
+ background-color:#000;
+ position : absolute;
+ width : 100%;
+ margin-bottom:0px;
+
+
+
+
+}
+.img
+{
+display : inline-block ;
+vertical-align : top;
+width : 50px;
+height : 50px;
+
+}
+</style>
+      </head>
+      <body>
+
+      <nav  class="navbar navbar-expand-md navbar-light">
+
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      		<img  class = "img" src="img/logo1.png">
+
+
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="btn df" class="nav-link" href="index.php">Home</a>
+              </li>
+
+
+              <li class="nav-item">
+                <a class="btn df" class="nav-link" href="contact.php">Contact Me</a>
+              </li>
+          </div>
+       </nav>
+     </body>
+     </html>
---
+++ Create-A-Basic-Banking-System-Project-Of-The-Sparks-Foundation-webdevelopment-main/sql/basic_bank_system_database.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 13, 2021 at 10:35 AM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 5.6.37
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `basic bank system database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `sno` int(3) NOT NULL,
+  `sender` text NOT NULL,
+  `receiver` text NOT NULL,
+  `balance` int(8) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`sno`, `sender`, `receiver`, `balance`, `datetime`) VALUES
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(3) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `balance` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
+(1, 'Meera Sharma', 'Meera14@gmail.com', 5500),
+(2, 'Shagun Gupta', 'shagun1031@gmail.com', 70000),
+(3, 'Megha Patil', 'megha@gmail.com', 40000),
+(4, 'Aryan Singh', 'aryan94@gmail.com', 570000),
+(5, 'Rashi Bajaj', 'rashi27@gmail.com', 80000),
+(6, 'Apurva patil', 'apurva14@gmail.com', 29000),
+(7, 'karan Kohli', 'karan249@gmail.com', 2150),
+(8, 'Riya Verma', 'riya10@gmail.com', 41825),
+(9, 'Riddham Patil', 'riddham23@gmail.com', 79900),
+(10, 'Nayra Gupta', 'nayra93@gmail.com', 60696);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `sno` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
